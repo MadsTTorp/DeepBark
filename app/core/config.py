2f69d65217f919cb06225387998f4b1e5f55bdfa
matt <mads-torp@hotmail.com>
2024-12-26 13:27:54 +0100

    Added config file with prompt and llm configurations

---
+++ app/core/config.py
@@ -0,0 +1,20 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+prompt_template = """Brug følgende stykker kontekst til at besvare spørgsmålet i slutningen. 
+Hvis du ikke kender svaret, så sig bare, at du ikke ved det, og prøv ikke at opdigte et svar.
+Svar med maksimalt tre sætninger og hold svaret så kortfattet men præcist som muligt.
+Vær høflig i dit svar.
+
+{context} 
+
+Spørgsmål: {question} 
+
+Hjælpsomt svar:"""
+
+custom_rag_prompt = PromptTemplate.from_template(prompt_template)
