8ec778a49208cc2901b171e261f077ddc90f6b7a
matt <mads-torp@hotmail.com>
2024-12-30 16:23:50 +0100

    Alligned prompt

--- app/core/config.py
+++ app/core/config.py
@@ -7,15 +7,13 @@ load_dotenv(find_dotenv())
 
 llm = ChatOpenAI(model="gpt-4o-mini")
 
-prompt_template = """Brug følgende stykker kontekst til at besvare spørgsmålet
-i slutningen. Hvis du ikke kender svaret, så sig bare, at du ikke ved det, og
-prøv ikke at opdigte et svar. Svar med maksimalt tre sætninger og hold svaret
-så kortfattet men præcist som muligt. Vær høflig i dit svar.
-
-{context}
-
-Spørgsmål: {question}
-
-Hjælpsomt svar:"""
+prompt_template = (
+    "Brug følgende stykker kontekst til at besvare spørgsmålet i "
+    "slutningen. Hvis du ikke kender svaret, så sig bare, at du "
+    "ikke ved det, og prøv ikke at opdigte et svar. Svar med "
+    "maksimalt tre sætninger og hold svaret så kortfattet men "
+    "præcist som muligt. Vær høflig i dit svar.\n\n"
+    "{context}\n\nSpørgsmål: {question}\n\nHjælpsomt svar:"
+)  
 
 custom_rag_prompt = PromptTemplate.from_template(prompt_template)
