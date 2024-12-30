b784781e6466fb723adc1bb16109f275836232dc
matt <mads-torp@hotmail.com>
2024-12-30 16:04:43 +0100

    Updated tests

--- tests/core/test_config.py
+++ tests/core/test_config.py
@@ -11,13 +11,13 @@ def test_environment_variables_loaded():
 
 def test_llm_initialization():
     assert isinstance(llm, ChatOpenAI)
-    assert llm.model == "gpt-4o-mini"
+    assert llm.model_name == "gpt-4o-mini"
 
 
 def test_llm_api_key():
     try:
         # Attempt to make a simple call to ensure the API key is valid
-        response = llm("Hello, how are you?")
+        response = llm.invoke("Hello, how are you?")
         assert response is not None
     except Exception as e:
         pytest.fail(f"Failed to init ChatOpenAI with the provided API key:{e}")
@@ -25,15 +25,11 @@ def test_llm_api_key():
 
 def test_prompt_template_initialization():
     assert isinstance(custom_rag_prompt, PromptTemplate)
-    expected_template = """Brug følgende stykker kontekst til at besvare
-spørgsmålet i slutningen. Hvis du ikke kender svaret, så sig bare,
-at du ikke ved det, og prøv ikke at opdigte et svar. Svar med maksimalt tre
-sætninger og hold svaret så kortfattet men præcist som muligt.
-Vær høflig i dit svar.
-
-{context}
-
-Spørgsmål: {question}
-
-Hjælpsomt svar:"""
+    expected_template = (
+    "Brug følgende stykker kontekst til at besvare spørgsmålet i slutningen. "
+    "Hvis du ikke kender svaret, så sig bare, at du ikke ved det, og prøv "
+    "ikke at opdigte et svar. Svar med maksimalt tre sætninger og hold svaret "
+    "så kortfattet men præcist som muligt. Vær høflig i dit svar.\n\n"
+    "{context}\n\nSpørgsmål: {question}\n\nHjælpsomt svar:"
+    )  # Updated expected template string
     assert custom_rag_prompt.template == expected_template
