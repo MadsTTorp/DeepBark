2ecd80b80dc6442e5ccce539ca21e3d8862551c9
matt <mads-torp@hotmail.com>
2025-01-02 16:22:23 +0100

    Added conditional check to collect source

--- app/services/rag_service.py
+++ app/services/rag_service.py
@@ -7,7 +7,9 @@ import logging
 logger = logging.getLogger(__name__)
 
 def get_rag_answer(question: str) -> dict:
+
     logger.info(f"Received question: {question}")
+    # try:
     
     # define query
     query = {"messages": [{"role": "user", "content": question}]}
@@ -37,21 +39,35 @@ def get_rag_answer(question: str) -> dict:
         ),
         None
     )
-    if tool_message:
+    try:
+        if tool_message:
+            source_list = []
+            for doc in tool_message.artifact:
+                if hasattr(doc, 'metadata') and 'source' in doc.metadata:
+                    source_list.append(doc.metadata['source'])
+                elif isinstance(doc, dict) and 'metadata' in doc and 'source' in doc['metadata']:
+                    source_list.append(doc['metadata']['source'])
+    
+        else:
+            source_list = []
+
+        # Remove duplicates
+        source_list = list(set(source_list))
+
+        # default response in case of no sources
+        if not source_list:
+            answer =  ('Jeg kender desværre ikke svaret på dit '
+                    'spørgsmål, på baggrund af de artikler '
+                    'jeg har adgang til.')
+
+        logger.info(f"Answer: {answer}, Sources: {source_list}")
+
+    except Exception as e:
         print(tool_message.artifact)
-        source_list = [doc.metadata['source'] for doc in tool_message.artifact]
-    else:
+        print('\n')
+        print(tool_message.artifact[0])
+        logger.error(f"Error: {e}")
+        answer = 'Noget gik galt, prøv venligst igen.'
         source_list = []
-
-    # Remove duplicates
-    source_list = list(set(source_list))
-
-    # default response in case of no sources
-    if not source_list:
-        answer =  ('Jeg kender desværre ikke svaret på dit '
-                  'spørgsmål, på baggrund af de artikler '
-                  'jeg har adgang til.')
-
-    logger.info(f"Answer: {answer}, Sources: {source_list}")
     
     return {"answer": answer, "sources": source_list}
